0421acaeca728d0bfe71545fdf574b4734f3ee50
fuchida-ytr <fuchida.ytr@gmail.com>
2021-05-26 00:37:30 +0900

    #11 create course show/edit/update/destroy

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,7 +8,30 @@ class CoursesController < ApplicationController
     def create
         @course = Course.new(course_params)
         @course.save
-        redirect_to courses_path, success: '投稿に成功しました！'
+        redirect_to courses_path, notice: '投稿に成功しました'
+    end
+
+    def show
+        @course = Course.find(params[:id])
+    end
+
+    def edit
+        @course = Course.find(params[:id])
+    end
+
+    def update
+        @course = Course.find(params[:id])
+        if @course.update(course_params)
+            redirect_to course_path(@course), notice: '更新しました'
+        else
+            render 'show'
+        end
+    end
+
+    def destroy
+        @course = Course.find(params[:id])
+        @course.destroy
+        redirect_to courses_path, notice: '削除しました'
     end
 
     private
