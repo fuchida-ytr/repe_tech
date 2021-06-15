893bc446c43d557cbb3af338ff6a29f3d798ce80
fuchida-ytr <fuchida.ytr@gmail.com>
2021-06-15 17:30:05 +0900

    #71 create review_sections index/show/correct/incorrect

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,13 +3,20 @@ class Course < ApplicationRecord
     has_many :chapters, dependent: :destroy
     attachment :image
 
+    # コース内の全section_idを配列で返す
+    def get_section_ids
+        self.chapters.map{ |chapter| chapter.sections.pluck(:id)}.flatten
+    end
+    
+    # ユーザーのコースの進捗率を返す
     def get_course_progress(user_id)
         # 完了したsection数 / 全section数
-        all_sectons_ids = Section.where(chapter_id: chapters.pluck(:id)).pluck(:id)
-        return 0 if all_sectons_ids.size == 0
-        # all_sectons_ids = chapters.map{ |chapter| chapter.sections.pluck(:id) }.flatten
-        comp_sections_ids = CompletedSection.where(user_id: user_id).where(section_id: all_sectons_ids).pluck(:id)
-        (100 * comp_sections_ids.size / all_sectons_ids.size).round
+        all_section_ids = Section.where(chapter_id: chapters.pluck(:id)).pluck(:id)
+        return 0 if all_section_ids.size == 0
+        # all_section_ids = chapters.map{ |chapter| chapter.sections.pluck(:id) }.flatten
+        comp_section_ids = CompletedSection.where(user_id: user_id).where(section_id: all_section_ids).pluck(:id)
+        (100 * comp_section_ids.size / all_section_ids.size).round
     end
+
 
 end
--- app/models/review_section.rb
+++ app/models/review_section.rb
@@ -2,6 +2,43 @@ class ReviewSection < ApplicationRecord
   belongs_to :user
   belongs_to :section
 
-  validates_uniqueness_of :section_id, scope: :user_id 
+  validates_uniqueness_of :section_id, scope: :user_id
 
+  # [{course: todays_section_num}]
+  def self.get_todays_review_list(user_id)
+    # 復習するセクションid
+    # 復習予定日が今日以前のsection_idを全取得
+    section_ids = self.where(user_id: user_id).where("next_review_date <= ?", Date.current).pluck(:section_id)
+
+    today_sections = Section.where(id: section_ids)
+    todays_review_list = {} # {course: section数,}
+    today_sections.each do |section|
+      todays_review_list[section.chapter.course] = todays_review_list[section.chapter.course].to_i + 1
+    end
+    todays_review_list
+  end
+
+  # 今日の復習するsectionをステージ別で1つランダムで返す
+  def self.get_todays_review_section(user_id, course, stage)
+
+    # 復習する全sectionを取得
+    user_review_sections = self.where(user_id: user_id).where(section_id: course.get_section_ids).where("next_review_date <= ?", Date.current)
+
+    # stage == "all"のときはそのまま
+    todays_review_sections = user_review_sections
+
+    if stage == "1"
+      # stage1は,review_stage_idがnil
+      todays_review_sections = user_review_sections.where(review_stage_id: nil)
+    elsif stage != "all"
+      # ユーザーが持つstageからstage名を検索してidを取得
+      current_user = User.find(user_id)
+      stage_id = current_user.review_stages.find_by(stage: stage).id
+      todays_review_sections = user_review_sections.where(review_stage_id: stage_id)
+    end
+
+    return nil if todays_review_sections.empty?      
+    # ランダムで1つsectionを返す
+    todays_review_sections.sample.section
+  end
 end
