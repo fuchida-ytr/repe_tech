cfc27a1a48f0dc7961fdb26464c1c543827cb1e2
fuchida-ytr <fuchida.ytr@gmail.com>
2021-07-17 01:42:59 +0900

    #97 Create search function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+
   def index
   end
+
+  # キーワードを含むセクション、記事を検索
+  def search
+    @sections = Section.search(params[:keyword]).limit(5)
+    @articles = Article.search(params[:keyword]).limit(5)
+    @keyword = params[:keyword]
+		# @diaries = Diary.search(params[:search]).order(id: :desc).page(params[:page]).per(9)
+	end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,12 +1,22 @@
 class Section < ApplicationRecord
+  # t.string  :title
+  # t.text    :content
+  # t.integer :chapter_id
+
   belongs_to :chapter
   has_many :completed_sections, dependent: :destroy
   has_many :review_sections,    dependent: :destroy
 
+  def self.search(keyword)
+    where(["title like? OR content like?", "%#{keyword}%", "%#{keyword}%"])
+  end
+
+  # 完了済みかチェック
   def completed_by?(user)
     completed_sections.where(user_id: user).exists?
   end
 
+  # 復習するかチェック
   def reviewed_by?(user)
     review_sections.where(user_id: user).exists?
   end
